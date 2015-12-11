72f78235ff6c7a818406cacb780b417e7250075c
rootsical <rootsical@gmail.com>
2015-12-11 19:24:45 +0000

    prepares for trying out gulp transpile task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,7 @@ import babel from 'gulp-babel';
 import plumber from 'gulp-plumber';
 import rename from 'gulp-regex-rename';
 
+
 gulp.task('transpile', function() {
   return gulp.src(['src/*.src.js', {base: 'src'})
     .pipe(plumber())
@@ -11,3 +12,10 @@ gulp.task('transpile', function() {
     .pipe(gulp.dest('dist'));
 });
 
+gulp.task('watch', function(){
+	gulp.watch(['src/*.src.js'], gulp.series('transpile'));
+});
+
+gulp.task('default', 
+	gulp.series('transpile', 'watch')
+);
