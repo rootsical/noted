d15caece992dd7cd874733a66e49116009390feb
rootsical <rootsical@gmail.com>
2015-12-11 17:33:05 +0000

    changes transpile task to incorporate rename of source files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,13 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import plumber from 'gulp-plumber';
+import rename from 'gulp-regex-rename';
 
 gulp.task('transpile', function() {
-	return gulp.src(['src/*.js'], {base:'src'})
-		.pipe(plumber())
-		.pipe(babel())
-		.pipe(gulp.dest('dest'));
+  return gulp.src(['src/*.src.js', {base: 'src'})
+    .pipe(plumber())
+    .pipe(babel())
+    .pipe(rename(/\.src\.js$/, '.js'))
+    .pipe(gulp.dest('dist'));
 });
 
