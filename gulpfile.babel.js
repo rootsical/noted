710e2cc56dbe1469bffaec9f8c968d346a87056a
rootsical <rootsical@gmail.com>
2015-12-11 22:43:20 +0000

    running electron and watching relevant files with gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,12 +3,12 @@ import babel from 'gulp-babel';
 import nodemon from 'gulp-nodemon';
 import plumber from 'gulp-plumber';
 import rename from 'gulp-regex-rename';
+import electronConnect from 'electron-connect';
 
-gulp.task('node', function() {
-	nodemon({
-		script: 'app/main.js',
-		watch: ['app']
-	});
+let electron = electronConnect.server.create({'path':'app'});
+
+gulp.task('start', function(){
+	electron.start();
 });
 
 gulp.task('transpile', function() {
@@ -19,12 +19,29 @@ gulp.task('transpile', function() {
     .pipe(gulp.dest('app'));
 });
 
+gulp.task('views', function(){
+	return gulp.src('src/views/*src.html', {base: 'src'})
+		.pipe(rename(/\.src\.html$/, '.html'))
+		.pipe(gulp.dest('app'))
+	;
+});
+
+gulp.task('manifest', function(){
+	return gulp.src('src/manifest/electron.package.json')
+		.pipe(rename(/electron.package.json/, 'package.json'))
+		.pipe(gulp.dest('app'))
+	;
+});
+
 gulp.task('watch', function(){
-	gulp.watch(['src/*.src.js'], gulp.series('transpile'));
+	gulp.watch(['src/*.src.js'], gulp.series('transpile', electron.restart));
+	gulp.watch(['src/views/*.src.html'], gulp.series('views', electron.reload));
+	gulp.watch(['src/manifest/electron.package.json'], gulp.series('manifest', electron.restart));
 });
 
 gulp.task('default', 
-	gulp.series('transpile',
-		gulp.parallel('watch', 'node')
+	gulp.series(
+		gulp.parallel('transpile','views', 'manifest'),
+		gulp.parallel('start', 'watch')
 	)
 );
