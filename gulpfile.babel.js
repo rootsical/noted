4b01d97f3ff73fe355d49cb2b8d7a4936f200d56
rootsical <rootsical@gmail.com>
2015-12-15 10:21:22 +0000

    changes to directory locations due to project restructure

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,47 +5,30 @@ import plumber from 'gulp-plumber';
 import rename from 'gulp-regex-rename';
 import electronConnect from 'electron-connect';
 
-let electron = electronConnect.server.create({
-  'path': 'app'
-});
+let electron = electronConnect.server.create();
 
 gulp.task('start', function() {
   electron.start();
 });
 
 gulp.task('transpile', function() {
-  return gulp.src(['src/*.src.js'], {
+  return gulp.src(['src/**/*.src.js'], {
       base: 'src'
     })
     .pipe(plumber())
     .pipe(babel())
     .pipe(rename(/\.src\.js$/, '.js'))
-    .pipe(gulp.dest('app'));
-});
-
-gulp.task('views', function() {
-  return gulp.src('src/views/*src.html', {
-      base: 'src'
-    })
-    .pipe(rename(/\.src\.html$/, '.html'))
-    .pipe(gulp.dest('app'));
-});
-
-gulp.task('manifest', function() {
-  return gulp.src('src/manifest/electron.package.json')
-    .pipe(rename(/electron.package.json/, 'package.json'))
-    .pipe(gulp.dest('app'));
+    .pipe(gulp.dest('lib'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch(['src/*.src.js'], gulp.series('transpile', electron.restart));
-  gulp.watch(['src/views/*.src.html'], gulp.series('views', electron.reload));
-  gulp.watch(['src/manifest/electron.package.json'], gulp.series('manifest', electron.restart));
+  gulp.watch(['src/**/*.src.js'], gulp.series('transpile', electron.restart));
+  gulp.watch(['views/**/*.html'], gulp.series(electron.reload));
 });
 
 gulp.task('default',
   gulp.series(
-    gulp.parallel('transpile', 'views', 'manifest'),
+    'transpile',
     gulp.parallel('start', 'watch')
   )
 );
