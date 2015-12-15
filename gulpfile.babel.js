a6e738d5eb311b5ae78dffebc379621c57e0d3bc
rootsical <rootsical@gmail.com>
2015-12-15 23:26:13 +0000

    test renderer gulp task added using electron-mocha

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,8 +4,11 @@ import nodemon from 'gulp-nodemon';
 import plumber from 'gulp-plumber';
 import rename from 'gulp-regex-rename';
 import electronConnect from 'electron-connect';
+import childProcess from 'child_process';
 
+let spawn = childProcess.spawn;
 let electron = electronConnect.server.create();
+
 
 gulp.task('start', function() {
   electron.start();
@@ -16,9 +19,38 @@ gulp.task('transpile', function() {
       base: 'src'
     })
     .pipe(plumber())
-    .pipe(babel())
+    .pipe(babel({
+      babelrc: false,
+      plugins: ['babel-plugin-transform-es2015-modules-commonjs']
+    }))
     .pipe(rename(/\.src\.js$/, '.js'))
     .pipe(gulp.dest('lib'));
+});
+
+gulp.task('test:renderer', function(done){
+
+  let electronMocha = spawn('electron-mocha', [
+    '--renderer',
+    '--require',
+    'babel-core/register', 
+    '--no-timeouts',
+    'test/renderer'
+  ]);
+
+  // electronMocha.stdout.setEncoding('utf8');
+  electronMocha.stdout.on('data', function (data) {
+    process.stdout.write(data.toString());
+  });
+
+  electronMocha.on('error', function (err) {
+    console.log('Failed to start child process.');
+  });
+
+  electronMocha.on('close', function (code, signal) {
+    // console.log('child process terminated with code: ' + code);
+    // console.log('child process terminated due to receipt of signal: ' + signal);
+    done();
+  });
 });
 
 gulp.task('watch', function() {
