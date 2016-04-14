eaa2229a4f4542745b125fbfbd97f2d9dd14867c
rootsical <rootsical@gmail.com>
2016-04-14 22:55:03 +0100

    gundb experimentation

--- docs/gundb.md
+++ docs/gundb.md
@@ -1,14 +1,56 @@
+#GunDB
+
+using gun-level v3.1.0 which uses gun v0.3.7
 
 install:  
-`npm install gun-level --save`
+```js
+npm install gun-level --save
+```
 
 to start a new database:  
-`var db = new Gun();`  
-the database will be created in project root at './level'.
+```js
+var db = new Gun();
+```  
+the database will be created in project root at *'./level'*.
 
-to change the location of the database do:
-```javascript
+if you would like a different location for the database do:  
+```js
 var db = new Gun({level: {path: 'db'}});
 ```
-the database will be created in project root './db'.
+the database will be created in project root at *'./db'*.
 
+add a node and index it with a key:  
+```js
+var hello = gun.put({
+  title: 'hello',
+  content: 'hello from gundb!'
+})
+.key('notes/hello', function(err){
+  if(err) console.log('error on setting key');
+  console.log('success on setting key');
+});
+```
+
+get a node by its key:
+```js
+var hello = db.get('notes/hello');
+```
+if the key doesn't exist, it will be created with an empty object assigned
+
+
+get a value from a node, without subscribing to changes:
+```js
+db.get('notes/hello').path('content')val(function(data){
+    console.log('val returns: ', data);
+});
+```
+
+using `set` to add a unique node to an unordered list:  
+```js
+var cool = gun.put({
+  type: 'comment',
+  title: 'cool',
+})
+db.get('notes/hello').path('comments').set(cool);
+```
+you can also create an empty set using `set()`.
--- test/main/gundb.test.js
+++ test/main/gundb.test.js
@@ -2,17 +2,32 @@ import expect from 'expect.js';
 import Gun from 'gun-level';
 import fs from 'fs-extra';
 
+let db = null;
 
 describe.only('gundb', function(){
 
   before(function(){
+    // create a database before carrying out tests
     // console.log('creating db');
-    var db = new Gun({level: {path: 'db'}});
+    db = new Gun({level: {path: 'db'}});
     // console.log('db created');
+    db.put({
+      title: 'hello note',
+      contents: 'hello from gundb!'
+    }).key('notes/hello', function(err){
+      if(err) {
+        console.log('error on setting key');
+      } else {
+        console.log('success on setting key');
+      }
+    });
+    // populate database with some data
+
   });
 
   after(function(){
     
+    // delete the database dir when tests are complete
     // console.log('removing db');
     fs.remove('db', function (err) {
       if (err) {
@@ -23,13 +38,39 @@ describe.only('gundb', function(){
     });
   });
 
-  it.only('creates a database', function(done){
+  it('creates a database', function(done){
     
+    // check that the db directory 
     fs.ensureDir('db', function(err){
       expect(err).not.to.be(true);
       done();
     });
 
+  });
+
+  it('returns a value from an existing node', function(done){
+    db.get('notes/hello').path('contents').val(function(data){
+      expect(data).to.equal('hello from gundb!');
+      done();
+    });
+  });
+
+  it('creates a new node', function(done){
+    db.put({
+      title: 'new note',
+      contents: 'this is a new note'
+    }).key('notes/new', function(err){
+      if(err) {
+        console.log('error on setting key');
+      } else {
+        console.log('success on setting key');
+      }
+    });
+
+    db.get('notes/new').path('contents').val(function(data){
+      expect(data).to.equal('this is a new note');
+      done();
+    });
   });
 
 });
